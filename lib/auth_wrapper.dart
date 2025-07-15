a91afb1176e9dca58545711cb13c467c88392e35
NguyenHoangBaoTran <trannhbse182378@fpt.edu.vn>
2025-07-15 15:25:08 +0700

    Update auth

--- lib/auth_wrapper.dart
+++ lib/auth_wrapper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'routes/app_routes.dart';
 
 class AuthWrapper extends StatefulWidget {
@@ -19,13 +20,24 @@ class _AuthWrapperState extends State<AuthWrapper> {
   Future<void> _checkAuth() async {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('token');
-    final role = int.tryParse(prefs.getString('role') ?? '');
 
     if (token != null && token.isNotEmpty) {
-      if (role == 1) {
-        Navigator.pushReplacementNamed(context, AppRoutes.designerHomepage);
-      } else {
-        Navigator.pushReplacementNamed(context, AppRoutes.customerHomepage);
+      try {
+        // Decode token để lấy role
+        final decodedToken = JwtDecoder.decode(token);
+        final role = decodedToken['Role'] ?? '';
+
+        if (role == 'Customer') {
+          Navigator.pushReplacementNamed(context, AppRoutes.customerHomepage);
+        } else if (role == 'Designer') {
+          Navigator.pushReplacementNamed(context, AppRoutes.designerHomepage);
+        } else {
+          // Nếu role không xác định
+          Navigator.pushReplacementNamed(context, AppRoutes.login);
+        }
+      } catch (e) {
+        // Token bị lỗi / hết hạn
+        Navigator.pushReplacementNamed(context, AppRoutes.login);
       }
     } else {
       Navigator.pushReplacementNamed(context, AppRoutes.login);
